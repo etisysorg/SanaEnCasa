3a71e0c3197a5b7aaf8eba58096a9ae20d7960fa
Andres <aguirregzz97@gmail.com>
2018-11-12 13:26:15 -0600

    minor

---
+++ client/src/shared/scrolling.js
@@ -0,0 +1,5 @@
+$(#).click(function() {
+   //optionally remove the 500 (which is time in milliseconds) of the
+   //scrolling animation to remove the animation and make it instant
+   $.scrollTo($('#myDiv'), 500);
+});
