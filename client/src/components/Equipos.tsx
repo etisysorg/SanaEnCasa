2507a959bc074b8ccc9cc6ceac0abd1706d1d39b
Andres <aguirregzz97@gmail.com>
2018-11-30 11:52:36 -0600

    think im done

--- client/src/components/Equipos.tsx
+++ client/src/components/Equipos.tsx
@@ -27,30 +27,72 @@ export default class Home extends React.Component<Props, State> {
                 <div className='row'>
                     <div className='col-lg-4 text-center'>
                         <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '30px', fontWeight: 'bold', paddingBottom: '10px' }} className='text-center'>Cama de hospital</h4>
-                        <img className='imgGrid' src={camaDeHospital} alt='' />
+                        <div style={{ height: '210px' }} className='imgWrap'>
+                            <img className='imgGrid' src={camaDeHospital} alt='' />
+                            <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '5px' }} className='text-center imgDescription'>
+                                Cuando un paciente esta inmovilizado una cama de hospital es
+                                indispensable, para asearlo, darle de comer, cambiarlo de posición. Contamos con camas
+                                de hospital manuales y eléctricas, dependiendo la necesidad del paciente
+                            </h4>
+                        </div>
                     </div>
                     <div className='col-lg-4 text-center'>
                         <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '30px', fontWeight: 'bold', paddingBottom: '10px' }} className='text-center'>Colchón antillagas</h4>
-                        <img className='imgGrid' src={colchonAntillagas} alt='' />
+                        <div style={{ height: '210px' }} className='imgWrap'>
+                            <img className='imgGrid' src={colchonAntillagas} alt='' />
+                            <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '5px' }} className='text-center imgDescription'>
+                                El colchón antillagas evita las dolorosas llagas en pacientes
+                                inmovilizados, estas llagas son fáciles de evitar, pero muy lentas para sanar
+                            </h4>
+                        </div>
                     </div>
                     <div className='col-lg-4 text-center'>
                         <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '30px', fontWeight: 'bold', paddingBottom: '10px' }} className='text-center'>Silla de ruedas</h4>
-                        <img className='imgGrid' src={sillaDeRuedas} alt='' />
+                        <div style={{ height: '210px' }} className='imgWrap'>
+                            <img className='imgGrid' src={sillaDeRuedas} alt='' />
+                            <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '5px' }} className='text-center imgDescription'>
+                                Una silla de ruedas que se adapta a las necesidades del paciente, le
+                                estas pueden ser: ligera si se trasladan a
+                                consultas, firme, compacta, con llantas grandes y brazos abatibles, para pacientes que
+                                pueden mover sus brazos, sillas especiales PCI
+                            </h4>
+                        </div>
                     </div>
                 </div>
 
-                <div style={{}} className='row'>
+                <div className='row'>
                     <div className='col-lg-4 text-center'>
                         <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '30px', fontWeight: 'bold', paddingBottom: '10px' }} className='text-center'>Grúa elevapacientes</h4>
-                        <img className='imgGrid' src={gruaElevapaciente} alt='' />
+                        <div style={{ height: '210px' }} className='imgWrap'>
+                            <img className='imgGrid' src={gruaElevapaciente} alt='' />
+                            <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '5px' }} className='text-center imgDescription'>
+                                Sin una grúa para movilizar a un paciente se requerirá de la ayuda de varias personas. El paciente muchas veces tiene
+                                que esperar para un cambio de posición o para su aseo hasta que el resto de la familia
+                                regrese de trabajar o un vecino pueda venir a ayudar
+                            </h4>
+                        </div>
                     </div>
                     <div className='col-lg-4 text-center'>
                         <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '30px', fontWeight: 'bold', paddingBottom: '10px' }} className='text-center'>Aspirador de secreciones</h4>
-                        <img className='imgGrid' src={aspiradorDeSecreciones} alt='' />
+                        <div style={{ height: '210px' }} className='imgWrap'>
+                            <img className='imgGrid' src={aspiradorDeSecreciones} alt='' />
+                            <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '5px' }} className='text-center imgDescription'>
+                                Cuando un paciente esta completamente inmovilizado y sus
+                                vías respiratorias están comprometidas, el aspirador de secreciones permite eliminar las
+                                mucosidades de una manera fácil y práctica
+                            </h4>
+                        </div>
                     </div>
                     <div className='col-lg-4 text-center'>
                         <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '30px', fontWeight: 'bold', paddingBottom: '10px' }} className='text-center'>Andadores</h4>
-                        <img className='imgGrid' src={andadora} alt='' />
+                        <div style={{ height: '210px' }} className='imgWrap'>
+                            <img className='imgGrid' src={andadora} alt='' />
+                            <h4 style={{ color: '#009ce0', fontFamily: 'Montserrat', paddingTop: '5px' }} className='text-center imgDescription'>
+                                Cuando un paciente va perdiendo fuerzas en sus extremidades, un andador
+                                permite que se sostenga y mantenga el equilibro mientras camina, le brinda
+                                independencia en su día a día. Pueden ser con banquito, llantitas o normales
+                            </h4>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/components/Testimonios.tsx
+++ client/src/components/Testimonios.tsx
@@ -15,20 +15,18 @@ export default class Home extends React.Component<Props, State> {
 
     render() {
         return (
-            <div id='testimonios' className='testimonios'>
+            <div style={{ paddingLeft: '40px', paddingRight: '40px' }} id='testimonios' className='testimonios'>
                 <h1 className='text-center' style={{ fontFamily: 'Montserrat', color: 'white', fontWeight: 'bold', paddingBottom: '15px', paddingTop: '30px' }}>TESTIMONIOS</h1>
-                <div style={{ paddingLeft: '40px', paddingRight: '40px' }} className='row'>
-                    <div className='col-md-3 text-center'>
+                <div className='row'>
+                    <div className='col-md-4 text-center'>
                         <img className='imgTestimonio' style={{ width: '250px' }} src={testimonioBenito} alt='' />
-                        <h4 className='text-center' style={{ color: 'white', lineHeight: '1.2' }}>
+                        <h4 style={{ color: 'white', lineHeight: '1.2' }}>
                             Testimonio de benito: Sana en casa le proporciono una ¡silla de ruedas scooter! esto le permitió tener mayor independencia y mejorar su calidad de vida
                         </h4>
                     </div>
-                    <div className='col-md-3'>
+                    <div className='col-md-4'>
                     </div>
-                    <div className='col-md-3'>
-                    </div>
-                    <div className='col-md-3'>
+                    <div className='col-md-4'>
                     </div>
                 </div>
             </div>
