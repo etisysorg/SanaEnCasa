6af1c251638de866da9ecfc7e1b3507ca95c63fb
aguirregzz97 <aguirregzz97@gmail.com>
2020-06-03 15:43:59 +0200

    Add boletines to navbar

--- client/src/components/NavbarC.tsx
+++ client/src/components/NavbarC.tsx
@@ -5,6 +5,12 @@ const logo = require('./../assets/img/sanaEnCasaLogoNavbar.png')
 const pdf1 = require('./../assets/img/BoletinSep2018.pdf')
 const pdf2 = require('./../assets/img/BoletinDiciembre2018.pdf')
 const pdf3 = require('./../assets/img/BoletinMarzo2019.pdf')
+const pdf4 = require('./../assets/img/BoletinJunio2019.pdf')
+const pdf5 = require('./../assets/img/BoletinSeptiembre2019.pdf')
+const pdf6 = require('./../assets/img/BoletinDiciembre2019.pdf')
+const pdf7 = require('./../assets/img/BoletinInformeAnual2019.pdf')
+const pdf8 = require('./../assets/img/BoletinMarzo2020.pdf')
+const pdf9 = require('./../assets/img/BoletinJunio2020.pdf')
 
 type State = {
     isOpen: boolean
@@ -61,13 +67,19 @@ export default class NavbarC extends React.Component<Props, State> {
                                 <a id='aporta' style={{ padding: '4px', fontFamily: 'Montserrat', color: 'white', fontWeight: 'bold', border: '2px solid white', borderRadius: '10px' }} className='navItems text-uppercase' href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LC2UM7J6DUZBJ&source=url'><i style={{ paddingRight: '3px' }} className='fas fa-wallet'></i>Aporta</a>
                             </li>
                             <li className='nav-item dropdown'>
-                                <a style={{ fontFamily: 'Montserrat', paddingRight: '10px' }} className='navItems text-uppercase dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown'>
+                                <a style={{ fontFamily: 'Montserrat', paddingRight: '20px' }} className='navItems text-uppercase dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown'>
                                     Boletines
                                 </a>
-                                <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
-                                    <a target='_blank' className='dropdown-item' href={pdf1}>SEP 2018</a>
-                                    <a target='_blank' className='dropdown-item' href={pdf2}>DIC 2018</a>
-                                    <a target='_blank' className='dropdown-item' href={pdf3}>MARZO 2019</a>
+                                <div style={{ marginTop: '10px' }} className='dropdown-menu dropdown-menu-right' aria-labelledby='navbarDropdown'>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf1 }>SEP 2018</a>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf2 }>DIC 2018</a>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf3 }>MARZO 2019</a>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf4 }>JUNIO 2019</a>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf5 }>SEPTIEMBRE 2019</a>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf6 }>DICIEMBRE 2019</a>
+                                    <a style={{ fontSize: '14px', fontWeight: 'bold' }} target='_blank' className='dropdown-item' href={ pdf7 }>INFORME ANUAL 2019</a>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf7 }>MARZO 2020</a>
+                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf8 }>JUNIO 2020</a>
                                 </div>
                             </li>
                         </ul>
