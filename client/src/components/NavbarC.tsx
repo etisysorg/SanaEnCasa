0d845300427930589d71a8f4cd6c6bcebe01a341
Andres <aguirregzz97@gmail.com>
2021-04-28 17:59:50 -0500

    Add new mission and informe anual 2020

--- client/src/components/NavbarC.tsx
+++ client/src/components/NavbarC.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
 
 const logo = require('./../assets/img/sanaEnCasaLogoNavbar.png')
 const pdf1 = require('./../assets/img/BoletinSep2018.pdf')
@@ -12,82 +11,238 @@ const pdf7 = require('./../assets/img/BoletinInformeAnual2019.pdf')
 const pdf8 = require('./../assets/img/BoletinMarzo2020.pdf')
 const pdf9 = require('./../assets/img/BoletinJunio2020.pdf')
 const pdf10 = require('./../assets/img/BoletinSeptiembre2020.pdf')
+const pdf11 = require('./../assets/img/BoletinInformeAnual2020.pdf')
 
 type State = {
-    isOpen: boolean
+  isOpen: boolean
 }
 
-type Props = {
-}
+type Props = {}
 
 export default class NavbarC extends React.Component<Props, State> {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            isOpen: false
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false,
     }
+  }
 
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
 
-    render() {
-        return (
-            <div className='Navbar'>
-                <nav id='navBackground' className='navbar navbar-expand-lg navbar-dark fixed-top'>
-                    <a className='text-uppercase navTitle' href='#home'><img id='logoHeader' src={logo} alt='' /></a>
-                    <button id='buttonCollapse' className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-                        <span className='navbar-toggler-icon'></span>
-                    </button>
+  render() {
+    return (
+      <div className='Navbar'>
+        <nav
+          id='navBackground'
+          className='navbar navbar-expand-lg navbar-dark fixed-top'
+        >
+          <a className='text-uppercase navTitle' href='#home'>
+            <img id='logoHeader' src={logo} alt='' />
+          </a>
+          <button
+            id='buttonCollapse'
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
 
-                    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                        <ul className='navbar-nav ml-auto'>
-                            <li className='nav-item'>
-                                <a style={{ fontFamily: 'Montserrat' }} className='navItems text-uppercase' href='#nosotros'>Nosotros</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a style={{ fontFamily: 'Montserrat' }} className='navItems text-uppercase' href='#queHacemos'>¿Qué hacemos?</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a style={{ fontFamily: 'Montserrat' }} className='navItems text-uppercase' href='#comoAyudar'>¿Cómo ayudar?</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a style={{ fontFamily: 'Montserrat' }} className='navItems text-uppercase' href='#equipoMedico'>Equipo médico</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a style={{ fontFamily: 'Montserrat' }} className='navItems text-uppercase' href='#testimonios'>Testimonios</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a style={{ fontFamily: 'Montserrat' }} className='navItems text-uppercase' href='#contactanos'>Contáctanos</a>
-                            </li>
-                            <li className='nav-item dropdown'>
-                                <a style={{ fontFamily: 'Montserrat', paddingRight: '20px' }} className='navItems text-uppercase dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown'>
-                                    Boletines
-                                </a>
-                                <div style={{ marginTop: '10px' }} className='dropdown-menu dropdown-menu-right' aria-labelledby='navbarDropdown'>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf1 }>SEP 2018</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf2 }>DIC 2018</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf3 }>MARZO 2019</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf4 }>JUNIO 2019</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf5 }>SEPTIEMBRE 2019</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf6 }>DICIEMBRE 2019</a>
-                                    <a style={{ fontSize: '14px', fontWeight: 'bold' }} target='_blank' className='dropdown-item' href={ pdf7 }>INFORME ANUAL 2019</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf8 }>MARZO 2020</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf9 }>JUNIO 2020</a>
-                                    <a style={{ fontSize: '14px' }} target='_blank' className='dropdown-item' href={ pdf10 }>SEPTIEMBRE 2020</a>
-                                </div>
-                            </li>
-                            <li className='nav-item'>
-                                <a id='aporta' style={{ padding: '4px', fontFamily: 'Montserrat', color: 'white', fontWeight: 'bold', border: '2px solid white', borderRadius: '10px' }} className='navItems text-uppercase' href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LC2UM7J6DUZBJ&source=url'><i style={{ paddingRight: '3px' }} className='fas fa-wallet'></i>Aporta</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        )
-    }
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item'>
+                <a
+                  style={{ fontFamily: 'Montserrat' }}
+                  className='navItems text-uppercase'
+                  href='#nosotros'
+                >
+                  Nosotros
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  style={{ fontFamily: 'Montserrat' }}
+                  className='navItems text-uppercase'
+                  href='#queHacemos'
+                >
+                  ¿Qué hacemos?
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  style={{ fontFamily: 'Montserrat' }}
+                  className='navItems text-uppercase'
+                  href='#comoAyudar'
+                >
+                  ¿Cómo ayudar?
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  style={{ fontFamily: 'Montserrat' }}
+                  className='navItems text-uppercase'
+                  href='#equipoMedico'
+                >
+                  Equipo médico
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  style={{ fontFamily: 'Montserrat' }}
+                  className='navItems text-uppercase'
+                  href='#testimonios'
+                >
+                  Testimonios
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  style={{ fontFamily: 'Montserrat' }}
+                  className='navItems text-uppercase'
+                  href='#contactanos'
+                >
+                  Contáctanos
+                </a>
+              </li>
+              <li className='nav-item dropdown'>
+                <a
+                  style={{ fontFamily: 'Montserrat', paddingRight: '20px' }}
+                  className='navItems text-uppercase dropdown-toggle'
+                  href='#'
+                  id='navbarDropdown'
+                  role='button'
+                  data-toggle='dropdown'
+                >
+                  Boletines
+                </a>
+                <div
+                  style={{ marginTop: '10px' }}
+                  className='dropdown-menu dropdown-menu-right'
+                  aria-labelledby='navbarDropdown'
+                >
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf1}
+                  >
+                    SEP 2018
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf2}
+                  >
+                    DIC 2018
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf3}
+                  >
+                    MARZO 2019
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf4}
+                  >
+                    JUNIO 2019
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf5}
+                  >
+                    SEPTIEMBRE 2019
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf6}
+                  >
+                    DICIEMBRE 2019
+                  </a>
+                  <a
+                    style={{ fontSize: '14px', fontWeight: 'bold' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf7}
+                  >
+                    INFORME ANUAL 2019
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf8}
+                  >
+                    MARZO 2020
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf9}
+                  >
+                    JUNIO 2020
+                  </a>
+                  <a
+                    style={{ fontSize: '14px' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf10}
+                  >
+                    SEPTIEMBRE 2020
+                  </a>
+                  <a
+                    style={{ fontSize: '14px', fontWeight: 'bold' }}
+                    target='_blank'
+                    className='dropdown-item'
+                    href={pdf11}
+                  >
+                    INFORME ANUAL 2020
+                  </a>
+                </div>
+              </li>
+              <li className='nav-item'>
+                <a
+                  id='aporta'
+                  style={{
+                    padding: '4px',
+                    fontFamily: 'Montserrat',
+                    color: 'white',
+                    fontWeight: 'bold',
+                    border: '2px solid white',
+                    borderRadius: '10px',
+                  }}
+                  className='navItems text-uppercase'
+                  href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LC2UM7J6DUZBJ&source=url'
+                >
+                  <i
+                    style={{ paddingRight: '3px' }}
+                    className='fas fa-wallet'
+                  ></i>
+                  Aporta
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
 }
--- client/src/components/Nosotros.tsx
+++ client/src/components/Nosotros.tsx
@@ -3,68 +3,177 @@ const logo = require('./../assets/img/logo.png')
 const mision = require('./../assets/img/mision.png')
 const videoLogo = require('./../assets/img/videoLogo.png')
 
+type State = {}
 
-type State = {
-}
-
-type Props = {
-}
+type Props = {}
 
 export default class Nosotros extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+  }
 
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div style={{ border: '4px dashed white' }} id='nosotros' className='nostros text-center'>
-                <h1 style={{ color: 'white', fontFamily: 'Montserrat', paddingTop: '30px', fontWeight: 'bold', paddingBottom: '20px' }} className='text-center'>NOSOTROS</h1>
-                <div className='row'>
-                    <div className='col-md-5'>
-                        <img id='imgLogoNosotros' src={logo} alt='' />
-                    </div>
-                    <div className='col-md-7'>
-                        <h3 className='text-center' style={{ paddingLeft: '40px', paddingRight: '40px', color: 'white', lineHeight: '1.5', paddingBottom: '30px' }}>
-                            Sana en casa es una asociación de beneficencia privada que surge a principios del 2015, con el objetivo de apoyar a familias que tienen un enfermo en casa y que por su situación económica no tienen los medios para comprar o rentar equipo médico
-                        </h3>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-md-8'>
-                        <h3 className='text-center' style={{ paddingLeft: '40px', paddingRight: '40px', color: 'white', lineHeight: '1.5', paddingBottom: '30px', paddingTop: '20px' }}>
-                            Principalmente prestamos: camas de hospital, sillas de ruedas, grúas eleva pacientes, aspiradores de secreciones y algunos otros equipos
-                        </h3>
-                    </div>
-                    <div className='col-md-4'>
-                        <a id='imgVideo' target='_blank' href='https://www.youtube.com/watch?v=XlokHSeEGKs'><img src={ videoLogo } style={{ paddingTop: '10px', width: '150px' }}/></a>
-                    </div>
-                </div>
-                <hr />
-                <div className='row'>
-                    <div className='col-md-4 text-center'>
-                        <span style={{ color: 'white', fontSize: '50px', paddingBottom: '10px' }} className='fas fa-hand-holding-heart'></span>
-                        <h2 style={{ color: 'white', fontFamily: 'Montserrat', fontWeight: 'bold' }} className='text-center'>Misión</h2>
-                        <h5 className='text-center' style={{ color: 'white', lineHeight: '1.4' }}>
-                            Ofrecer apoyo a las familias que tienen un enfermo en casa y que por su situación económica o social no cuentan con los medios necesarios para atenderlos
-                        </h5>
-                    </div>
-                    <div className='col-md-4 text-center'>
-                        <img style={{ width: '65px', paddingLeft: '10px', paddingBottom: '10px' }} className='text-center' src={mision} alt=''/>
-                        <h2 style={{ color: 'white', fontFamily: 'Montserrat', fontWeight: 'bold' }} className='text-center'>Visión</h2>
-                        <h5 className='text-center' style={{ color: 'white', lineHeight: '1.4' }}>
-                            Llegar a ser una asociación con una sólida estructura organizacional que se mantenga en constante crecimiento y que proporcione apoyo cada vez a un mayor numero de familias
-                        </h5>
-                    </div>
-                    <div className='col-md-4 text-center'>
-                        <span style={{ color: 'white', fontSize: '50px', paddingBottom: '10px' }} className='fas fa-cross'></span>
-                        <h2 style={{ color: 'white', fontFamily: 'Montserrat', fontWeight: 'bold' }} className='text-center'>Valores</h2>
-                        <h5 className='text-center' style={{ color: 'white', lineHeight: '1.4' }}>
-                            CARIDAD para los más necesitados, SOLIDARIO ante las difíciles situaciones, IGUALDAD para brindar apoyo a quien más lo necesite y buscando siempre la TRASPARENCIA en sus actividades
-                        </h5>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        style={{ border: '4px dashed white' }}
+        id='nosotros'
+        className='nostros text-center'
+      >
+        <h1
+          style={{
+            color: 'white',
+            fontFamily: 'Montserrat',
+            paddingTop: '30px',
+            fontWeight: 'bold',
+            paddingBottom: '20px',
+          }}
+          className='text-center'
+        >
+          NOSOTROS
+        </h1>
+        <div className='row'>
+          <div className='col-md-5'>
+            <img id='imgLogoNosotros' src={logo} alt='' />
+          </div>
+          <div className='col-md-7'>
+            <h3
+              className='text-center'
+              style={{
+                paddingLeft: '40px',
+                paddingRight: '40px',
+                color: 'white',
+                lineHeight: '1.5',
+                paddingBottom: '30px',
+              }}
+            >
+              Sana en casa es una asociación de beneficencia privada que surge a
+              principios del 2015, con el objetivo de apoyar a familias que
+              tienen un enfermo en casa y que por su situación económica no
+              tienen los medios para comprar o rentar equipo médico
+            </h3>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-md-8'>
+            <h3
+              className='text-center'
+              style={{
+                paddingLeft: '40px',
+                paddingRight: '40px',
+                color: 'white',
+                lineHeight: '1.5',
+                paddingBottom: '30px',
+                paddingTop: '20px',
+              }}
+            >
+              Principalmente prestamos: camas de hospital, sillas de ruedas,
+              grúas eleva pacientes, aspiradores de secreciones y algunos otros
+              equipos
+            </h3>
+          </div>
+          <div className='col-md-4'>
+            <a
+              id='imgVideo'
+              target='_blank'
+              href='https://www.youtube.com/watch?v=XlokHSeEGKs'
+            >
+              <img
+                src={videoLogo}
+                style={{ paddingTop: '10px', width: '150px' }}
+              />
+            </a>
+          </div>
+        </div>
+        <hr />
+        <div className='row'>
+          <div className='col-md-4 text-center'>
+            <span
+              style={{
+                color: 'white',
+                fontSize: '50px',
+                paddingBottom: '10px',
+              }}
+              className='fas fa-hand-holding-heart'
+            ></span>
+            <h2
+              style={{
+                color: 'white',
+                fontFamily: 'Montserrat',
+                fontWeight: 'bold',
+              }}
+              className='text-center'
+            >
+              Misión
+            </h2>
+            <h5
+              className='text-center'
+              style={{ color: 'white', lineHeight: '1.4' }}
+            >
+              Contribuir al ejercicio del derecho a la salud y la seguridad
+              social de las familias de bajos recursos que tienen un enfermo en
+              casa
+            </h5>
+          </div>
+          <div className='col-md-4 text-center'>
+            <img
+              style={{
+                width: '65px',
+                paddingLeft: '10px',
+                paddingBottom: '10px',
+              }}
+              className='text-center'
+              src={mision}
+              alt=''
+            />
+            <h2
+              style={{
+                color: 'white',
+                fontFamily: 'Montserrat',
+                fontWeight: 'bold',
+              }}
+              className='text-center'
+            >
+              Visión
+            </h2>
+            <h5
+              className='text-center'
+              style={{ color: 'white', lineHeight: '1.4' }}
+            >
+              Llegar a ser una asociación con una sólida estructura
+              organizacional que se mantenga en constante crecimiento y que
+              proporcione apoyo cada vez a un mayor numero de familias
+            </h5>
+          </div>
+          <div className='col-md-4 text-center'>
+            <span
+              style={{
+                color: 'white',
+                fontSize: '50px',
+                paddingBottom: '10px',
+              }}
+              className='fas fa-cross'
+            ></span>
+            <h2
+              style={{
+                color: 'white',
+                fontFamily: 'Montserrat',
+                fontWeight: 'bold',
+              }}
+              className='text-center'
+            >
+              Valores
+            </h2>
+            <h5
+              className='text-center'
+              style={{ color: 'white', lineHeight: '1.4' }}
+            >
+              CARIDAD para los más necesitados, SOLIDARIO ante las difíciles
+              situaciones, IGUALDAD para brindar apoyo a quien más lo necesite y
+              buscando siempre la TRASPARENCIA en sus actividades
+            </h5>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
